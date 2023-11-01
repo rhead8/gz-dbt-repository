0416f92930b3f739732aa681ffe5b31374adf225
rheariaan@gmail.com <rheariaan@gmail.com>
2023-11-01 13:00:47 +0000

    evolution for finance team

---
+++ models/finance_days.sql
@@ -0,0 +1,10 @@
+SELECT
+date_date
+, COUNT (orders_id) AS nb_transactions
+, ROUND (SUM (revenue),1) AS revenue
+, ROUND (AVG (revenue),1) AS average_basket
+, ROUND (SUM (margin),1) AS margin
+, ROUND (SUM (operational_margin),1) AS operational_margin
+FROM {{ref("int_orders_operational")}}
+GROUP BY  date_date
+ORDER BY  date_date DESC
